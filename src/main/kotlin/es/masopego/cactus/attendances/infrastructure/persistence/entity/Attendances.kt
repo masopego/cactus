3ff652ca285fc8e5eeb9392e710a6d7345aa0763
masopego <masopego@gmail.com>
2025-11-23 13:24:54 +0100

    fix: update Attendances entity to reference Meetup

--- src/main/kotlin/es/masopego/cactus/attendances/infrastructure/persistence/entity/Attendances.kt
+++ src/main/kotlin/es/masopego/cactus/attendances/infrastructure/persistence/entity/Attendances.kt
@@ -1,13 +1,16 @@
 package es.masopego.cactus.attendances.infrastructure.persistence.entity
 
+import es.masopego.cactus.meetups.infrastructure.persistence.entity.Meetups
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.javatime.datetime
 import java.util.*
 
 object Attendances : Table("attendance") {
     val id = uuid("id").clientDefault { UUID.randomUUID() }
+
+    //TODO Update user as reference when User entity is created
     val user = uuid("user")
-    val meetup = uuid("meetup")
+    val meetup = reference("meetup", Meetups.id)
     val confirmed = datetime("confirmed").nullable()
 
     override val primaryKey = PrimaryKey(id)
